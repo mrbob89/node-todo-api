97ffc63beb42f298f9795b7ff4215afcdb9c213d
Roman Boboshko <romanboboshko89@gmail.com>
2017-01-25 02:34:24 +0200

    Add delete examples

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -6,9 +6,15 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     }
     console.log('Connected to MongoDB server');
 
-    db.collection('Todos').find({completed: false}).toArray().then((docs) => {
-        console.log('Todos');
-        console.log(JSON.stringify(docs, undefined, 2));
+    // db.collection('Todos').find({completed: false}).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    db.collection('Todos').find().count().then((count) => {
+        console.log(`Todos count: ${count}`);
     }, (err) => {
         console.log('Unable to fetch todos', err);
     });
