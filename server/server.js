a39deaf7bbb92420a0b4cc4e7707b9485ad415ce
Roman Boboshko <romanboboshko89@gmail.com>
2017-01-28 00:11:27 +0200

    Save data to the database using mongoose

--- server/server.js
+++ server/server.js
@@ -5,22 +5,47 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
 var newTodo = new Todo({
-    text: 'Cook dinner'
+    text: 'One more dinner'
 });
 
 newTodo.save().then((doc) => {
-    console.log('Save todo', doc);
+    console.log('Save todo', JSON.stringify(doc, undefined, 2));
 }, (e) => {
     console.log('Unable to save todo');
+});
+
+// User
+var User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    }
+});
+
+var newUser = new User({
+    email: 'user@test.com'
+});
+
+newUser.save().then((doc) => {
+    console.log('Save user:', JSON.stringify(doc, undefined, 2));
+}, (e) => {
+    console.log('Unable to save user');
 });
